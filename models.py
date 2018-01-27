6749a4309848ed00a8397525900423e76c0a80d2
Amir <amir.anwar.said@gmail.com>
2018-01-27 15:12:28 +0200

    finished modeling the database

--- models.py
+++ models.py
@@ -1,53 +1,91 @@
 import os
 from peewee import SqliteDatabase
 from peewee import Model
-from peewee import CharField, BlobField, BooleanField, IntegerField, ForeignKeyField
+from peewee import (CharField, BlobField, 
+                    BooleanField, ForeignKeyField,
+                    TextField,DateField,
+                    DateTimeField,)
 
 db = SqliteDatabase('data.db')
 
-class Klass(Model):
+class BaseModel(Model):
+    class Meta:
+        database = db    
+
+class Stage(BaseModel):
     name = CharField()
-    parent_stage = CharField()
+
+    def __repr__(self):
+        return 'Stage object: ' + self.name
+
+class Klass(BaseModel):
+    id = CharField()
+    stage = ForeignKeyField(Stage, related_name='classes')
     
     def __repr__(self):
-        return 'Klass object: ' + self.name + '/' +self.parent_stage  
+        return 'Klass object: ' + self.id + '/' +self.stage  
     
-    class Meta:
-        database = db
-
-class Sheep(Model):
+class Sheep(BaseModel):
     name = CharField()
     klass = ForeignKeyField(Klass ,related_name="sheeps")
+    picture = BlobField()
+    address = TextField()
+    mobile = CharField(max_length=22)
+    home_number = CharField(max_length=22)
+    father_number = CharField(max_length=22)
+    mother_number = CharField(max_length=22)
+    grandparent_number = CharField(max_length=22)
+    other_number_1 = CharField(max_length=22)
+    other_number_2 = CharField(max_length=22)
+    other_address = TextField()
+    confession_father = CharField()
+    birthday = DateField()
+    school = CharField()
+    email = CharField()
+    comments = TextField()
+
 
     def __repr__(self):
         return 'Sheep object: ' + self.name
-    class Meta:
-        database = db
 
-class Servant(Model):
+class Servant(BaseModel):
     name = CharField()
     klass = ForeignKeyField(Klass, related_name="servants")
     leader_of = ForeignKeyField(Klass, related_name="leader")
-    
+    is_admin  = BooleanField()
+
     def __repr__(self):
         return 'Servant object: ' + self.name
     
-    class Meta:
-        database = db
 
+class Visit(BaseModel):
+    sheep = ForeignKeyField(Sheep, related_name='visits')
+    servants = TextField()
+    visit_date = DateTimeField()
+    comments = TextField()
+    
+    def __repr__(self):
+        return 'Visit object, to: ' + self.sheep.name + ' on: ' + self.visit_date
+    
+class Attendance(BaseModel):
+    class_id = ForeignKeyField(Klass, related_name='attendances')
+    date = DateField()
 
-class Visit(Model):
-    pass
+    def __repr__(self):
+        return 'Attendance object for class: ' + self.class_id + ' on: ' + self.date
 
-class Attendance(Model):
-    pass
-
+class AttendanceRecord(BaseModel):
+    sheep = ForeignKeyField(Sheep)
+    attendance = ForeignKeyField(Attendance)
 
 # Append this list whenever you add new models
 TABLES_LIST = [
     Klass,
     Sheep,
     Servant,
+    Visit, 
+    Attendance,
+    AttendanceRecord,
     ]
 
 def main():
