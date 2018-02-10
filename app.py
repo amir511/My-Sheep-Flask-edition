06ec55e47051c876eae860ba73bec16907b37d92
Amir <amir.anwar.said@gmail.com>
2018-02-10 18:06:00 +0200

    added main urls and main layout of the application

--- app.py
+++ app.py
@@ -9,6 +9,45 @@ def home():
     title = 'Home'
     return render_template('home.html',title=title)
 
+@app.route('/login')
+def login():
+    pass
+
+@app.route('/logout')
+def logout():
+    pass
+
+@app.route('/new_stage')
+def add_stage():
+    pass
+
+@app.route('/new_class')
+def add_class():
+    pass
+    
+@app.route('/new_child')
+def add_child():
+    pass
+
+@app.route('/new_servant')
+def add_servant():
+    pass
+
+@app.route('/new_attendance')
+def add_attendance():
+    pass
+
+@app.route('/new_visit/<child_name>')
+def add_visit():
+    pass
+
+@app.route('/dashboard')
+def dashboard():
+    pass
+
+@app.route('/config')
+def site_configuration():
+    pass
 
 def main():
     app.debug = True
