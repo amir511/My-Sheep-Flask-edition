150f29082c8721ddf77e3dcd07012783183925bd
Amir <amir.anwar.said@gmail.com>
2018-02-24 12:27:56 +0200

    adjusted views layout, added new ones

--- app.py
+++ app.py
@@ -3,47 +3,135 @@ from flask import render_template
 
 app = Flask(__name__, template_folder='templates')
 
+
+######################################################
+# Home, login, logout views                          #
+######################################################
 @app.route('/home')
 @app.route('/')
 def home():
-    title = 'Home'
+    title = 'My Sheep|Home'
     return render_template('home.html',title=title)
 
 @app.route('/login')
 def login():
-    pass
+    title = 'My Sheep|Login'
+    return render_template('login.html', title=title)
 
 @app.route('/logout')
 def logout():
-    pass
+    title = 'My Sheep|Logout'
+    return render_template('logout.html', title=title)
 
-@app.route('/new_stage')
+
+######################################################    
+# The add new 'stuff' views                          #
+######################################################
+@app.route('/new/stage')
 def add_stage():
-    pass
+    title = 'My Sheep|new-stage'
+    return render_template('add_edit_stage.html', title=title)
 
-@app.route('/new_class')
+@app.route('/new/class')
 def add_class():
-    pass
+    title  = 'My Sheep|new-class'
+    return render_template('add_edit_class.html', title=title)
     
-@app.route('/new_child')
+@app.route('/new/child')
 def add_child():
-    pass
+    title  = 'My Sheep|new-child'
+    return render_template('add_edit_child.html', title=title)
 
-@app.route('/new_servant')
+@app.route('/new/servant')
 def add_servant():
-    pass
+    title  = 'My Sheep|new-servant'
+    return render_template('add_edit_servant.html', title=title)
 
-@app.route('/new_attendance')
+@app.route('/new/attendance/<class_id>')
 def add_attendance():
-    pass
+    title  = 'My Sheep|new-attendance'
+    return render_template('add_edit_attendance.html', title=title)
 
-@app.route('/new_visit/<child_name>')
+@app.route('/new/visit/<child_id>')
 def add_visit():
-    pass
+    title  = 'My Sheep|new-visit'
+    return render_template('add_edit_visit.html', title=title)
 
+
+######################################################    
+# The view 'stuff' views                             #
+######################################################
+@app.route('/view/stage/<stage_id>')
+def view_stage():
+    title  = 'My Sheep|view stage'
+    return render_template('view_stage.html', title=title)
+
+@app.route('/view/class/<class_id>')
+def view_class():
+    title  = 'My Sheep|view class'
+    return render_template('view_class.html', title=title)
+
+@app.route('/view/child/<child_id>')
+def view_child():
+    title  = 'My Sheep|view child'
+    return render_template('view_child.html', title=title)
+
+@app.route('/view/servant/<servant_id>')
+def view_servant():
+    title  = 'My Sheep|view servant'
+    return render_template('view_servant.html', title=title)
+
+@app.route('/view/attendance/<class_id>/<attendance_id>')
+def view_attendance():
+    title  = 'My Sheep|view attendance'
+    return render_template('view_attendance.html', title=title)
+
+@app.route('/view/visit/<child_id>/<visit_id>')
+def view_visit():
+    title  = 'My Sheep|view visit'
+    return render_template('view_visit.html', title=title)
+
+######################################################    
+# The edit 'stuff' views                             #
+######################################################
+@app.route('/edit/stage/<stage_id>')
+def edit_stage():
+    title  = 'My Sheep|edit stage'
+    return render_template('add_edit_stage.html', title=title)
+
+@app.route('/edit/class/<class_id>')
+def edit_class():
+    title  = 'My Sheep|edit class'
+    return render_template('add_edit_class.html', title=title)
+
+@app.route('/edit/child/<child_id>')
+def edit_child():
+    title  = 'My Sheep|edit child'
+    return render_template('add_edit_child.html', title=title)
+
+@app.route('/edit/servant/<servant_id>')
+def edit_servant():
+    title  = 'My Sheep|edit servant'
+    return render_template('add_edit_servant.html', title=title)
+
+@app.route('/edit/attendance/<class_id>/<attendance_id>')
+def edit_attendance():
+    title  = 'My Sheep|edit attendance'
+    return render_template('add_edit_attendance.html', title=title)
+
+@app.route('/edit/visit/<child_id>/<visit_id>')
+def edit_visit():
+    title  = 'My Sheep|edit visit'
+    return render_template('add_edit_visit.html', title=title)
+
+
+######################################################    
+# dashboard and site configuration views             #
+######################################################
 @app.route('/dashboard')
 def dashboard():
-    pass
+    title  = 'My Sheep|my dashboard'
+    return render_template('dashboard.html', title=title)
 
 @app.route('/config')
 def site_configuration():
